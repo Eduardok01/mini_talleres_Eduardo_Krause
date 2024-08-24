9a1afc381bb70bbcd0a23b4290a6e83b12315f3c
Eduardok01 <e.krause01@ufromail.cl>
2024-08-24 16:27:33 -0400

    add valores de muestra
    add (funcion1): se agregaron valores para el calculo de operaciones
    
    Se agregaron variables que contienen valores numericos para ser utilizados en funciones posteriores
    
    BREAKING CHANCE:
    las variables estan instanciadas utilizando int, valores decimales podrian causar errores

--- funcion1.cs
+++ funcion1.cs
@@ -1,3 +1,11 @@
 static void Main(string[] args){
     Console.write("hola mundo");
+
+    int valor1 = 4;
+    int valor2 = 8;
+
+    int resultado = valor1 +valor2;
+    Console.write(resultado);
+
+
 }
